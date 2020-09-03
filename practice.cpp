45f9e1ceeed999bb2d5e2c0db3ebb5acb0d86376
Rashmi Bachani <echorashmi@gmail.com>
2020-09-03 10:39:38 -0400

    Debugging Quick Sort Three Way

--- practice.cpp
+++ practice.cpp
@@ -1,33 +1,10 @@
 #include <iostream>
 #include <stdio.h>
 
-void swap(int *num_one, int *num_two)
-{
-    int tmp = *num_one;
-    *num_one = *num_two;
-    *num_two = tmp;
-}
-
-void swap_array_digits(int *arr)
-{
-    int tmp = arr[0];
-    arr[0] = arr[1];
-    arr[1] = tmp;
-}
 
 int main(void)
 {
-    //Swapping Ints inside an Array:
-    int a[] = {2, 5};
-    printf("a[0]: %i and a[1]: %i\n", a[0], a[1]);
-    swap_array_digits(a);
-    printf("a[0]: %i and a[1]: %i\n", a[0], a[1]);
-    
-    /*
-    //Swapping Ints
-    int a = 2, b = 5;
-    printf("a: %i and b: %i\n", a, b);
-    swap(&a, &b);
-    printf("a: %i and b: %i\n", a, b);
-    */
+    int arr[3] = {1, 2, 3};
+    arr = {4, 5, 6};
+    printf("%i", arr[1]);
 }
--- quick_sort.cpp
+++ quick_sort.cpp
@@ -14,7 +14,13 @@ typedef struct midpoints_struct{
     int midpoint_two;
 }midpoints_struct;
 
-const int data_length = 99999;
+/*
+    Decimal: 99998 | Binary: 11000011010011110 | Hex: 1869E
+    Decimal: 99999 | Binary: 11000011010011111 | Hex: 1869F
+    Decimal: 100000 | Binary: 11110100001001000000 | Hex: F4240
+*/
+
+const int data_length = 10;
 int dataset[data_length];
 
 int partition2(int *data, int left, int right) 
@@ -96,6 +102,7 @@ bool compare_two_arrays(int *array_one, int *array_two, int array_length)
     {
         if(array_one[i] != array_two[i])
         {
+            std::cout << i << "\t" << array_one[i] << "\t" << array_two[i] << "\n\n";
             return false;
         }
     }
@@ -105,10 +112,12 @@ bool compare_two_arrays(int *array_one, int *array_two, int array_length)
 
 void generate()
 {
+    time_t t;
+    srand((unsigned) time(&t));
     //Random Number Generator:
     int lower = 1;
-    int upper = 1000000000;
-    std::cout << "\nUpper Limit: " << upper;
+    int upper = 500;
+    //std::cout << "\nUpper Limit: " << upper;
     for (int i = 1; i <= data_length; i++)
     {
         int random_number = (rand() % (upper - lower + 1)) + lower;
@@ -116,47 +125,102 @@ void generate()
     }
 }
 
-int main() 
-{
-    /*
-    int n;
-    std::cin >> n;
-    int data[n];
-    for (int i = 0; i < n; ++i) 
+bool check_if_sorted(int *input_array, int array_size)
+{    
+    for(int i = 0; i < array_size - 1 ; i++) 
     {
-        std::cin >> data[i];
+        //printf("\n Comparing: %i with %i", input_array[i], input_array[i+1]);
+        if(input_array[i] > input_array[i+1])
+        {
+            printf("\n\n\nArray is not sorted at: %i \n", i);
+            return false;
+        }
     }
-    */
-    int array_length   = data_length;
-    int data_three_way[array_length];
-    int data_two_way[array_length];
-    int use_threeway_partition;
-    generate();
-    
-    for (int i = 0; i < array_length; i++) 
-    {
-        data_three_way[i] = dataset[i];
-        data_two_way[i]   = dataset[i];
-    }
-   
-    randomized_quick_sort(data_three_way, 0, array_length - 1, use_threeway_partition = 1);
-    randomized_quick_sort(data_two_way, 0, array_length - 1, use_threeway_partition = 0);
-
-    std::cout << "\n\nComparision Result: " << compare_two_arrays(data_three_way, data_two_way, array_length);
-    
-    /*
-    std::cout << "\n\nThree Way: \n";
-    for (size_t i = 0; i < array_length; ++i) 
-    {
-        std::cout << dataset[i] << ' ';
-    }
-
-    std::cout << "\n\nTwo Way: \n";
-    for (size_t i = 0; i < array_length; ++i) 
-    {
-        std::cout << dataset[i] << ' ';
-    }
-    */
-    
-    std::cout << "\n\n";
+    printf("\nArray is sorted\n");
+    return true;
 }
+
+int main(int argc, char * argv[]) 
+{
+    bool set_debug_mode = false;
+    if(argv[1] && (strcmp(argv[1], "debug") == 0))
+    {
+        set_debug_mode = true;
+    }
+    
+    if(set_debug_mode)
+    {
+        const int array_length   = data_length;
+        int data_two_way[array_length];
+        int use_threeway_partition;
+        
+        int data_three_way[array_length] = {0, 315, 402, 355, 27, 322, 391, 322, 119, 80};
+        /*
+        int data_three_way[array_length];
+        //Generate a Test Data Set with Random Numbers:
+        generate();
+        
+        //Assign the numbers to the 2 test arrays:
+        for (int i = 0; i < array_length; i++) 
+        {
+            data_three_way[i] = dataset[i];
+            data_two_way[i]   = dataset[i];
+        }
+        */
+
+        std::cout << "\n\nOriginal Array: \n";
+        for(int i = 0; i < array_length; i++)
+        {
+            std::cout << data_three_way[i] << "\t";
+        }
+
+        //Call Quick Sort using Three Way Partition: 
+        randomized_quick_sort(data_three_way, 0, array_length - 1, use_threeway_partition = 1);
+
+        //Call Quick Sort using Two Way Partition:
+        //randomized_quick_sort(data_two_way, 0, array_length - 1, use_threeway_partition = 0);
+
+        check_if_sorted(data_three_way, array_length);
+
+        //Output the Comparision Result of Two Way v/s Three Way Partition. 
+        //std::cout << "\n\nComparision Result: " << compare_two_arrays(data_three_way, data_two_way, array_length);
+        
+        std::cout << "\n\nArray After Sorting: \n";
+        for(int i = 0; i < array_length; i++)
+        {
+            std::cout << data_three_way[i] << "\t";
+        }
+
+        std::cout << "\n\n";
+    }
+    else
+    {
+        int use_threeway_partition;
+        int n;
+        std::cin >> n;
+        int data[n];
+        for (int i = 0; i < n; ++i) 
+        {
+            std::cin >> data[i];
+        }
+
+        randomized_quick_sort(data, 0, n - 1, use_threeway_partition = 1);
+        for (size_t i = 0; i < n; ++i) 
+        {
+            std::cout << data[i] << ' ';
+        }
+    }
+}
+
+
+
+
+/*
+    Array is not sorted at: 6 
+
+    Original Array: 
+    0	315	402	355	27	322	391	322	119	80	
+
+    Array After Sorting: 
+    0	27	80	119	315	322	391	322	355	402	
+*/
